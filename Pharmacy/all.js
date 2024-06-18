9db55e18e4a7ac3d707fec38172751ea99576cd1
Mohitrawal7 <rawalmohit12@gmail.com>
2024-06-18 15:07:31 +0545

    Pharmacy swiper  03/04

--- Pharmacy/all.js
+++ Pharmacy/all.js
@@ -1,8 +1,29 @@
 const sideBar= document.querySelector("#sidebar");
 const menu =document.querySelector("#menu");
-
+const dropDown = document.querySelector("#dropdown");
+const hasChildren = document.querySelector("#hasChildren"); 
 
 sideBar.addEventListener("click",()=>{
     menu.classList.toggle("hidden")
     sideBar.classList.toggle("white")
+});
+
+hasChildren.addEventListener("click",()=>{
+    dropDown.classList.toggle("hidden")
 })
+
+//swiper 
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
