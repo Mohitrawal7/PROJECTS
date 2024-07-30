c39839708d4e8e89c0526d5d0ae2ba73a59ee259
Mohitrawal7 <rawalmohit12@gmail.com>
2024-07-30 19:59:22 +0545

    new project i completed some days ago but could not publish it

---
+++ Movieland/src/App.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+
+import MovieCard from './MovieCard'
+
+import './App.css'
+import  SearchIcon from './search.svg'
+// b1fdf3f5
+const API_URL = 'https://www.omdbapi.com?apikey=b1fdf3f5'
+
+const App = () => {
+const [movies,setMovies] = useState([]);
+const [searchTerm,setSearchTerm] = useState("");
+
+useEffect(() =>{
+  searchMovies('anaconda'); 
+},[]);
+
+
+  const searchMovies = async (title) =>{
+    const response = await fetch(`${API_URL}&s=${title}`)
+const data = await response.json()
+
+setMovies(data.Search);
+  }; 
+
+  return (
+    <div className="app">
+    <h1>MovieLand</h1>
+
+    <div className="search">
+    <input
+    placeholder="Search for movies" 
+    value={searchTerm}
+    onChange={(e) => setSearchTerm(e.target.value)
+    }
+    />
+    <img src={SearchIcon} 
+    alt ="search" 
+    onClick={() => searchMovies(searchTerm) }
+    />
+    </div>
+
+    { movies?.length>0
+     ?(
+       <div className="container">
+        {movies.map((movie) =>(
+          <MovieCard movie={movie} />
+           ))}
+        </div>
+      ):(
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+
+    </div>
+  )
+}
+
+export default App
